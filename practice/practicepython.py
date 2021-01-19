494868db4e8a768f1863cc25cf89edc276c1afb5
kon6443@gmail.com <kon6443@gmail.com>
2021-01-19 00:15:26 -0600

    python first practice

---
+++ practice/practicepython.py
@@ -0,0 +1,20 @@
+print('='*30 + '\n')
+
+a = list()
+print(a)
+
+print()
+
+b = [1, 2, 'abc', 'def'] 
+print(b) 
+print(b[2]) 
+
+print()
+
+c = [1, 2, [3, 4]] 
+#print(c[0][0]) # 오류 
+print(c[2][1]) 
+print(c[-1])
+
+
+print('='*30)
