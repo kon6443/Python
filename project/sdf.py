f488816549af30ba439e9d2aabdf992f158ef1b0
kon6443@gmail.com <kon6443@gmail.com>
2021-02-10 18:50:13 -0600

    intro

---
+++ project/sdf.py
@@ -0,0 +1,13 @@
+import art
+
+def introduction():
+    art.tprint("Hello World!")
+    art.tprint("W E L C O M E !")
+    print("This is a CS552 Computer Science Capstone project in ESU.")
+    print("Made by Camaron Edgecomb, Draven George, Hao Lan, Israel Guerra Loza, Junyeong Jang, Onam Kwon, and Zihui Chen.")
+
+
+def init():
+    introduction()
+
+init()
