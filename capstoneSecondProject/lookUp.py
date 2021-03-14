fb02681fdd831b64cb641fa361ff11e150b5a69d
kon6443@gmail.com <kon6443@gmail.com>
2021-03-14 15:44:29 -0500

    capstone second project pre-lookup

--- capstoneSecondProject/lookUp.py
+++ capstoneSecondProject/lookUp.py
@@ -1,23 +1,42 @@
 import keyboard
 import csv
+import time
+
+global indexNum  #In order to compare each elemtn's letter one by one with keystroke
+indexNum = -1
 
 def keyStroke():
  temp = keyboard.read_key()
+ time.sleep(0.1)
+ global indexNum
+ indexNum += 1
  return temp
 
 def init(): 
  f = open('./DataBase.csv')
  data = csv.reader(f)
- indexNum = 0  #In order to compare each elemtn's letter one by one with keystroke
+ getKeyStroke = [] 
+ matchingFlag = False
+ global indexNum
 
- while True:
-  #print("while indexNum: ", indexNum) # testing while loop
-  getKeyStroke = keyStroke()
-  for row in data:
-   #print("for indexNum: ", indexNum) # testing for loop
-   if row[9][indexNum].lower() == getKeyStroke :
-    print(row[9])
-  indexNum += 1
+ while True: # looping in order to keep getting keystrokes
+  getKeyStroke.append(keyStroke())
+  print(getKeyStroke)
+  print("getKeyStroke: ", getKeyStroke[indexNum])
+  for row in data: # each row is an each cell in a csv file
+   matchingFlag = False
+   print("range(indexNum): ", range(indexNum+1))
+   print("indexNum: ", indexNum)
+   for x in range(indexNum+1):  # each x is an each letter in an each cell
+    print("row[9][indexNum]:", row[9][indexNum])
+    print("row[9][x]:", row[9][x])
+    if row[9][x].lower() == getKeyStroke[indexNum]:
+     matchingFlag = True
+     break
+    else: 
+     matchingFlag = False
+     break
+   if matchingFlag == True : print(row[9])
  f.close()
 
 init()
